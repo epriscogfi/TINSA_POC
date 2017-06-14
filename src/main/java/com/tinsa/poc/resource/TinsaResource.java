34d63dafbc85f04ac6f71bfb756990e9dd918ce6
eprisco <epriscom@gmail.com>
2017-06-14 09:40:09 +0200

    Devolucion code=201

--- src/main/java/com/tinsa/poc/resource/TinsaResource.java
+++ src/main/java/com/tinsa/poc/resource/TinsaResource.java
@@ -2,10 +2,12 @@ package com.tinsa.poc.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tinsa.poc.proxy.Mensaje;
@@ -33,6 +35,7 @@ public class TinsaResource {
 	 * @return the resultado notificar
 	 */
 	@RequestMapping(value="/notificar", method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
 	public @ResponseBody ResultadoNotificar notificar(
 			@RequestParam(value="destino") String destino, 
 			@RequestParam(value="tipoMensaje") String tipoMensaje, 
